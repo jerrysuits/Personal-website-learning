55558a9f1ac3bbc1dfb1946ca22db02bbaa92ad1
gerald <adudagerald@gmail.com>
2023-03-10 21:30:53 +0300

    phone number

---
+++ script.js
@@ -0,0 +1,39 @@
+const studentName = "Gerald"
+const technicalMentorName = "Beatrice"
+
+const lineOne = "Are we to speak, first day of the week";
+const lineTwo = "Stumbling words at the bar";
+const lineThree = "Beauty blue eyes, my order of fries";
+const lineFour = "Long Island kindness and wine";
+
+//console.log(lineOne, lineTwo, lineThree, lineFour);
+
+//nodeconst newLine = `${lineOne}/n ${lineTwo}/n ${lineThree}/n ${lineFour}/n`;
+
+//console.log(newLine)
+
+
+//let adultAge = 18;
+
+//if (adultAge >=29){
+    //console.log("allow them to drink")
+//} else {
+   // console.log("they are not allowed to drink")
+//}
+//arrays have values
+
+
+let car = "Fiat" // variable amd it has stored a string datatype of the car
+
+let carColour = "Purple"
+let carSize = "4"
+
+let carModel = {carColour: "Purple", carSize: "4", car: "Fiat"};
+console.log(carModel);
+console.log(carModel["carColour"]);
+
+
+
+
+
+
